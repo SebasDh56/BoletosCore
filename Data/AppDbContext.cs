343cfba6d9af5d5b661582f0ccc8767c9f0c7800
Amable Sebastian Quishpe <105821062+SebasDh56@users.noreply.github.com>
2025-05-28 19:29:23 -0500

    Crear contexto de DB

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BoletosCore.Models;
+
+namespace BoletosCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Pasajero> Pasajeros { get; set; }
+        public DbSet<Boleto> Boletos { get; set; }
+        public DbSet<Bus> Buses { get; set; }
+        public DbSet<Cooperativa> Cooperativas { get; set; }
+        public DbSet<BoletoRedirigido> BoletosRedirigidos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pasajero>()
+                .HasIndex(p => p.Cedula)
+                .IsUnique();
+        }
+    }
+}
